47b3931a7a45076b50ba855bbd5c779131cfabb9
mechanikate <cjmatthy200@gmail.com>
2025-02-23 09:40:23 -0500

    add arguments to only install packages via pacman if prompted to

--- installscript.sh
+++ installscript.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 PBG='\033[45m'
 RST='\033[0m'
-if [[ "$(which cava)" =~ "not found" ]]; then 
+usage() { echo "Usage: $0 [-i <yes|no>]" 1>&2; exit 1; }
+
+while getopts "i:" o; do
+    case $o in
+        i)
+            i=${OPTARG}
+	    [[ "$i" =~ ([Yy]([Ee][Ss])?)|([Nn]([Oo])?) ]] || usage
+            ;;
+       h | *)
+	    i="N"
+            ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+if [[ "$(which cava)" =~ "not found" ]]; then  # check for cava
 	echo -e "$PBG Uh oh! You don't have cava installed. Use an AUR package manager for https://github.com/karlstav/cava , or build it from source. $RST"
 	exit 1
 fi
 
-echo -e "$PBG Installing packages ...$RST"
-sudo pacman -S stow i3-wm feh polybar neovim zsh grep sudo hyfetch git
+if [[ "$i" =~ [Yy]([Ee][Ss])? ]]; then
+	echo -e "$PBG We will install the necessary packages (since -i is $i). $RST"
+	sudo pacman -S stow i3-wm feh polybar neovim zsh grep sudo hyfetch git
+fi
+
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 echo -e "$PBG Let's move this over to ~/.config/ ...$RST"
